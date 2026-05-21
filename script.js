e866a9764f3b1b39a844d1fcefa8a46f6d29a6e9
Matheus Barbosa <peteredu22015@gmail.com>
2026-05-21 15:47:07 -0300

    catalogo 3d

---
+++ script.js
@@ -0,0 +1,577 @@
+const products = [
+
+  {
+    id: 1,
+    name: "Arganel Flor de Lis",
+    category: "Tradicionais",
+    price: 8,
+
+    images: [
+      "img/flordelis1.png",
+      "img/fdl2.png",
+      "img/fdl3.png",
+      "img/fdl4.png",
+      "img/fdl5.png"
+    ],
+
+    description: "Arganel de Flor de Lis - Cores Personalizadas de Acordo com Preferência do cliente e Disponibilidade de Cores."
+  },
+
+  {
+    id: 2,
+    name: "Arganel Flor de Lis 2",
+    category: "Tradicionais",
+    price: 8,
+
+    images: [
+      "img/2fdl1.png",
+      "img/2fdl2.png",
+      "img/2fdl3.png",
+      "img/2fdl4.png",
+      "img/2fdl5.png"
+    ],
+
+    description: "Arganel de Flor de Lis - Cores Personalizadas de Acordo com Preferência do cliente e Disponibilidade de Cores."
+  },
+
+  {
+    id: 3,
+    name: "Fúria da Noite de Lenço",
+    category: "Personagens com Lenço",
+    price: 20,
+
+    images: [
+      "img/furiadelenço.png"
+    ],
+
+    description: "Personagem personalizado com o Lenço na cor do seu Grupo Escoteiro."
+  },
+
+  {
+    id: 4,
+    name: "Fúria da Luz de Lenço",
+    category: "Personagens com Lenço",
+    price: 20,
+
+    images: [
+      "img/furiadelenço2.png"
+    ],
+
+    description: "Personagem personalizado com o Lenço na cor do seu Grupo Escoteiro."
+  },
+
+  {
+    id: 5,
+    name: "Corujinha Escoteira",
+    category: "Animais com Lenço",
+    price: 15,
+
+    images: [
+      "img/corujadelenço1.png",
+      "img/corujadelenço2.png",
+      "img/corujadelenço3.png"
+    ],
+
+    description: "Corujinha personalizada com o Lenço na cor do seu Grupo Escoteiro."
+  },
+
+  {
+    id: 6,
+    name: "Rosto Fúria da Noite",
+    category: "Personagens",
+    price: 15,
+
+    images: [
+      "img/furiadanoite.png"
+    ],
+
+    description: "Personagem Fúria da Noite."
+  },
+
+  {
+    id: 7,
+    name: "Rosto Fúria da Luz",
+    category: "Personagens",
+    price: 15,
+
+    images: [
+      "img/furiadaluz.png"
+    ],
+
+    description: "Personagem Fúria da Luz."
+  },
+
+  {
+    id: 8,
+    name: "Plantinha",
+    category: "Tradicionais",
+    price: 15,
+
+    images: [
+      "img/plantinha.png"
+    ],
+
+    description: "Plantinha."
+  },
+
+  {
+    id: 9,
+    name: "Coelhinho",
+    category: "Animais",
+    price: 15,
+
+    images: [
+      "img/coelho.png"
+    ],
+
+    description: "Coelhinho - Cor Personalizada de Acordo com Preferência do cliente e Disponibilidade de Cores."
+  },
+
+  {
+    id: 10,
+    name: "Arganel de Patrulha",
+    category: "Tradicionais",
+    price: 15,
+
+    images: [
+      "img/agmorcego.png",
+      "img/agdourado.png",
+      "img/agcastor.png",
+      "img/agprata.png"
+    ],
+
+    description: "Arganel com SVG personalizado de acordo com a Patrulha Escolhida."
+  },
+
+  {
+    id: 11,
+    name: "Arganel Corvo",
+    category: "Tradicionais",
+    price: 15,
+
+    images: [
+      "img/corvo.png"
+    ],
+
+    description: "Arganel personalizado com tema Corvo."
+  },
+
+  {
+    id: 12,
+    name: "Darth Vader Escoteiro",
+    category: "Personagens com Lenço",
+    price: 20,
+
+    images: [
+      "img/darthvader.png"
+    ],
+
+    description: "Darth Vader personalizado com o Lenço na cor do seu Grupo Escoteiro."
+  },
+
+  {
+    id: 13,
+    name: "Gomu Gomu",
+    category: "Personagens",
+    price: 15,
+
+    images: [
+      "img/gomugomu.png"
+    ],
+
+    description: "Gomu Gomu - One Piece."
+  },
+
+  {
+    id: 14,
+    name: "Creeper",
+    category: "Personagens",
+    price: 15,
+
+    images: [
+      "img/creeper.png"
+    ],
+
+    description: "Creeper - Minecraft."
+  },
+
+  {
+    id: 15,
+    name: "Anel de Gilwell",
+    category: "Tradicionais",
+    price: 18,
+
+    images: [
+      "img/gilwell.png"
+    ],
+
+    description: "Anel de Gilwell Personalizado na cor do lenço do seu grupo."
+  },
+
+  {
+    id: 16,
+    name: "Snoopy de Chapéu",
+    category: "Personagens",
+    price: 20,
+
+    images: [
+      "img/snoopydechapeu.png"
+    ],
+
+    description: "Arganel do Snoopy com Chapéu."
+  },
+
+  {
+    id: 17,
+    name: "Snoopy de Lenço",
+    category: "Personagens com Lenço",
+    price: 20,
+
+    images: [
+      "img/snoopydelenço.png"
+    ],
+
+    description: "Snoopy personalizado com o Lenço na cor do seu Grupo Escoteiro."
+  },
+
+  {
+    id: 18,
+    name: "Lobo de Lenço",
+    category: "Animais com Lenço",
+    price: 15,
+
+    images: [
+      "img/lobo1.png",
+      "img/lobo2.png"
+    ],
+
+    description: "Lobo personalizado com o Lenço na cor do seu Grupo Escoteiro."
+  },
+
+  {
+    id: 19,
+    name: "Sonic",
+    category: "Personagens",
+    price: 20,
+
+    images: [
+      "img/sonic.png"
+    ],
+
+    description: "Sonic."
+  },
+
+  {
+    id: 20,
+    name: "Charmander de Lenço",
+    category: "Personagens com Lenço",
+    price: 25,
+
+    images: [
+      "img/charmander.png"
+    ],
+
+    description: "Charmander personalizado com o Lenço na cor do seu Grupo Escoteiro."
+  },
+
+  {
+    id: 21,
+    name: "Pikachu de Lenço",
+    category: "Personagens com Lenço",
+    price: 25,
+
+    images: [
+      "img/pikachu.png"
+    ],
+
+    description: "Pikachu personalizado com o Lenço na cor do seu Grupo Escoteiro."
+  },
+
+  {
+    id: 22,
+    name: "Bulbassauro de Lenço",
+    category: "Personagens com Lenço",
+    price: 25,
+
+    images: [
+      "img/bulba.png"
+    ],
+
+    description: "Bulbassauro personalizado com o Lenço na cor do seu Grupo Escoteiro."
+  },
+
+  {
+    id: 23,
+    name: "Squirtle de Lenço",
+    category: "Personagens com Lenço",
+    price: 25,
+
+    images: [
+      "img/squirtle.png"
+    ],
+
+    description: "Squirtle personalizado com o Lenço na cor do seu Grupo Escoteiro."
+  },
+
+  {
+    id: 24,
+    name: "Eevee de Lenço",
+    category: "Personagens com Lenço",
+    price: 25,
+
+    images: [
+      "img/eevee.png"
+    ],
+
+    description: "Eevee personalizado com o Lenço na cor do seu Grupo Escoteiro."
+  },
+
+  {
+    id: 25,
+    name: "Rayquaza",
+    category: "Personagens",
+    price: 18,
+
+    images: [
+      "img/rayquaza.png"
+    ],
+
+    description: "Rayquaza - Pokemon."
+  },
+
+  {
+    id: 26,
+    name: "Arganel Sharingan",
+    category: "Personagens",
+    price: 15,
+
+    images: [
+      "img/sharingan.png"
+    ],
+
+    description: "Mangekyou Sharingan - Sasuke."
+  },
+
+  {
+    id: 27,
+    name: "Personalizado",
+    category: "Personalizáveis",
+    price: 30,
+
+    images: [
+      "img/perso.png"
+    ],
+
+    description: "Entre em contato para fazer seu Personalizado - consultar orçamento sem compromisso."
+  },
+
+  {
+    id: 28,
+    name: "Capivara de Lenço",
+    category: "Animais com Lenço",
+    price: 20,
+
+    images: [
+      "img/capivaradelenço.png"
+    ],
+
+    description: "Capivara personalizada com o Lenço na cor do seu Grupo Escoteiro."
+  }
+
+]
+
+const productsContainer =
+  document.getElementById("productsContainer")
+
+const searchInput =
+  document.getElementById("searchInput")
+
+const categoryFilter =
+  document.getElementById("categoryFilter")
+
+const sortFilter =
+  document.getElementById("sortFilter")
+
+function renderProducts(productsList) {
+
+  productsContainer.innerHTML = ""
+
+  productsList.forEach(product => {
+
+    const whatsappMessage =
+      encodeURIComponent(
+        `Olá! Tenho interesse no ${product.name}`
+      )
+
+    const card = document.createElement("div")
+
+    card.classList.add("product-card")
+
+    card.innerHTML = `
+
+      <div class="carousel">
+
+        <button class="carousel-btn prev">
+          ❮
+        </button>
+
+        <img
+          src="${product.images[0]}"
+          class="product-image"
+        >
+
+        <button class="carousel-btn next">
+          ❯
+        </button>
+
+      </div>
+
+      <div class="product-info">
+
+        <span class="category">
+          ${product.category}
+        </span>
+
+        <h2 class="product-title">
+          ${product.name}
+        </h2>
+
+        <p class="product-description">
+          ${product.description}
+        </p>
+
+        <div class="product-footer">
+
+          <span class="price">
+            R$ ${product.price}
+          </span>
+
+          <a
+            href="https://wa.me/5561995193085?text=${whatsappMessage}"
+            target="_blank"
+            class="whatsapp-btn"
+          >
+            <i class="bi bi-whatsapp"></i>
+          </a>
+
+        </div>
+
+      </div>
+
+    `
+
+    const image =
+      card.querySelector(".product-image")
+
+    const prevBtn =
+      card.querySelector(".prev")
+
+    const nextBtn =
+      card.querySelector(".next")
+
+    let currentImage = 0
+
+    nextBtn.addEventListener("click", () => {
+
+      currentImage++
+
+      if(currentImage >= product.images.length) {
+        currentImage = 0
+      }
+
+      image.src =
+        product.images[currentImage]
+
+    })
+
+    prevBtn.addEventListener("click", () => {
+
+      currentImage--
+
+      if(currentImage < 0) {
+        currentImage =
+          product.images.length - 1
+      }
+
+      image.src =
+        product.images[currentImage]
+
+    })
+
+    productsContainer.appendChild(card)
+
+  })
+
+}
+
+function filterProducts() {
+
+  let filtered = [...products]
+
+  const search =
+    searchInput.value.toLowerCase()
+
+  filtered = filtered.filter(product =>
+    product.name
+      .toLowerCase()
+      .includes(search)
+  )
+
+  const category =
+    categoryFilter.value
+
+  if(category !== "Todos") {
+
+    filtered = filtered.filter(product =>
+      product.category === category
+    )
+
+  }
+
+  const sort =
+    sortFilter.value
+
+  if(sort === "low") {
+
+    filtered.sort((a,b) =>
+      a.price - b.price
+    )
+
+  }
+
+  if(sort === "high") {
+
+    filtered.sort((a,b) =>
+      b.price - a.price
+    )
+
+  }
+
+  if(sort === "name") {
+
+    filtered.sort((a,b) =>
+      a.name.localeCompare(b.name)
+    )
+
+  }
+
+  renderProducts(filtered)
+
+}
+
+searchInput.addEventListener(
+  "input",
+  filterProducts
+)
+
+categoryFilter.addEventListener(
+  "change",
+  filterProducts
+)
+
+sortFilter.addEventListener(
+  "change",
+  filterProducts
+)
+
+renderProducts(products)
